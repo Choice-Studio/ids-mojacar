7a2745dcc25860af0570d5aac40853cde8c33558
Jacob Carter <jcarterfreelance@outlook.com>
2024-03-01 17:21:35 +0000

    Added project template page

---
+++ src/components/ProjectGallery.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="wrapper grid grid-cols-1 gap-2">
+    <img
+      :key="image"
+      v-for="image in project.projectImages"
+      :src="'/project-images/' + image + '?url'"
+      :alt="'A picture of ' + project.projectTitle + ' interior design.'"
+      class="w-full h-auto"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import projectData from "../data/projectData.json";
+
+export default {
+  name: "ProjectGallery",
+  props: {
+    projectID: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      project: projectData[this.projectID],
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
