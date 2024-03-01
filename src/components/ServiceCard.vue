a8bca7c80a0bf2ed95436416b847f4f9a79df393
Jacob Carter <jcarterfreelance@outlook.com>
2024-03-01 19:10:56 +0000

    Adjusted services section for tablets

--- src/components/ServiceCard.vue
+++ src/components/ServiceCard.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="wrapper grid grid-cols-1 gap-8 text-center">
-    <div class="">
+  <div
+    class="w-full wrapper flex flex-col gap-8 text-center md:justify-end md:items-centre md:gap-16"
+    :class="[
+      imagePosition == 'right'
+        ? 'md:flex-row-reverse text-right'
+        : 'md:flex-row md:text-left',
+    ]"
+  >
+    <div class="md:w-1/2">
       <img
         :src="'/project-images/' + imageHref + '.jpeg?url'"
         alt="A picture of a house front porch. Walls are painted blue and white."
         class="aspect-square w-full"
       />
     </div>
-    <div class="grid grid-cols-1 gap-8 place-items-center">
+    <div
+      class="flex flex-col items-center justify-center gap-8 md:w-1/2"
+      :class="[imagePosition == 'right' ? 'md:items-end' : 'md:items-start']"
+    >
       <h3 class="text-4xl">
         {{ title }}
       </h3>
@@ -29,6 +39,10 @@ export default {
     body: String,
     buttonText: String,
     buttonHref: String,
+    imagePosition: {
+      type: String,
+      default: "right",
+    },
   },
 };
 </script>
